e4fc726db09edb2e278918b49915c41915c5a780
Richard Wattenbarger <github@wattenbarger.net>
2022-02-23 10:11:48 -0500

    added command-line args to control dimensions

--- ukraine.py
+++ ukraine.py
@@ -1,22 +1,54 @@
-from PIL import Image, ImageColor, ImageDraw, ImageFont
+import argparse
+
+from PIL import Image, ImageColor, ImageDraw
 
 STRONG_AZURE = '#0057b7'
-YELLOW = '#ffd700'
+YELLOW       = '#ffd700'
 
-with Image.new("RGBA", (400, 400), (255, 255, 255, 0)) as im:
+def process_args():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-o', '--output', default='ukraine.png')
+    parser.add_argument('-w', '--width', type=int, default=400)
+    parser.add_argument('-h', '--height', type=int, default=400)
 
-    d = ImageDraw.Draw(im)
+    return parser.parse_args()
 
-    d.pieslice(xy=[0.0, 0.0, 400.0, 400.0],
-               start=180.0,
-               end=360.0,
-               fill=ImageColor.getrgb(STRONG_AZURE))
+def main():
 
-    d.pieslice(xy=[0.0, 0.0, 400.0, 400.0],
-               start=0.0,
-               end=180.0,
-               fill=ImageColor.getrgb(YELLOW))
+    args = process_args()
 
-    im.save('ukraine.png')
+    width = args.width
+    height = args.height
+    output_filename = args.output
+
+    with Image.new("RGBA", (width, height), (255, 255, 255, 0)) as im:
+
+        d = ImageDraw.Draw(im)
+
+        horizontal_center = width // 2
+        vertical_center   = height // 2
+        diameter          = min(width, height)
+        radius            = diameter // 2
+        left_boundary     = horizontal_center - radius
+        right_boundary    = horizontal_center + radius
+        top_boundary = vertical_center - radius
+        bottom_boundary = vertical_center + radius
+        bounds = [(left_boundary, top_boundary), 
+                  (right_boundary, bottom_boundary)]
+
+        d.pieslice(xy=bounds, 
+                   start=180.0,
+                   end=360.0,
+                   fill=ImageColor.getrgb(STRONG_AZURE))
+
+        d.pieslice(xy=bounds,
+                   start=0.0,
+                   end=180.0,
+                   fill=ImageColor.getrgb(YELLOW))
+
+        im.save(output_filename)
+
+if __name__ == '__main__':
+    main()
 
 
